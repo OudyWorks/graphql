42e28e372776607253af181cd74e3b77ca82b66e
Ayoub Oudmane <ayoub@oudmane.me>
2018-04-30 19:44:01 +0100

    # getEntityType, GraphQLObjectType without id

--- source/getEntityType.js
+++ source/getEntityType.js
@@ -40,6 +40,18 @@ export default function getEntityType(ObjectType) {
                 case 'GraphQLList':
                     type[key] = Array
                     break
+
+                case 'GraphQLObjectType':
+
+                    let _config = getConfig(config[key].type)
+
+                    if(_config.id)
+                        type[key] = String
+
+                    else
+                        type[key] = getEntityType(config[key].type)
+
+                    break
                 
                 default:
                     type[key] = String
