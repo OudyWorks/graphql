79b70fe852dd7def7d3c0c1db9ce1a4377201b80
Ayoub Oudmane <ayoub@oudmane.me>
2018-12-18 22:13:38 +0300

    #

--- source/Entity/index.js
+++
@@ -1,8 +0,0 @@
-import Entity from '@oudyworks/entity'
-import extender from './extender'
-
-const GraphQLEntity = extender(Entity)
-
-GraphQLEntity.graphql = Symbol('graphql')
-
-export default GraphQLEntity
